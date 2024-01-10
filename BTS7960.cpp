19221493f7c85b0ad05bf3f827fed9e9a42424e4
Raihanysfr <154678891+Raihanysfr@users.noreply.github.com>
2024-01-10 15:10:23 +0700

    Add files via upload

--- BTS7960.cpp
+++ BTS7960.cpp
@@ -11,13 +11,21 @@ BTS7960::BTS7960(byte PWMA, byte PWMB)
 
 void BTS7960::drive(int16_t magnitude)
 {
-if (magnitude>0){
-  analogWrite(PWM_A, abs(magnitude));
-  analogWrite(PWM_B, 0);
+  constrain(PWM_A, -255, 255);
+  constrain(PWM_B, -255, 255);
+  if (magnitude>0)
+  {
+    analogWrite(PWM_A, abs(magnitude));
+    analogWrite(PWM_B, 0);
   }
-else if (magnitude<0)
-{
-  analogWrite(PWM_A, 0);
-  analogWrite(PWM_B, abs(magnitude));
+  else if (magnitude<0)
+  {
+    analogWrite(PWM_A, 0);
+    analogWrite(PWM_B, abs(magnitude));
+  }
+  else
+  {
+    analogWrite(PWM_A, 0);
+    analogWrite(PWM_B, 0);
   }
 }
